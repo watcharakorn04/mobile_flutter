3a1a7d4fdff9baaf8a6278d1f47d3a68b619faf4
watcharakorn04 <watcharakornc19@gmail.com>
2025-12-15 10:24:47 +0700

    week2_2

--- week2_1/test/widget_test - Copy.dart
+++ week2_1/test/widget_test - Copy.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:week2_1/main.dart';
+import 'package:week2_1/main_pic.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
